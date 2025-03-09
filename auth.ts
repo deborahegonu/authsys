ed10975eeb54d501c786eb213f8234beb0dfb853
deborahegonu <135129672+deborahegonu@users.noreply.github.com>
2025-03-09 14:27:41 +0100

    update auth.ts
    user session persists

--- auth.ts
+++ auth.ts
@@ -73,6 +73,18 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     strategy: "jwt",
     ...authConfig
   },
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) { // User is available during sign-in
+        token.id = user.id
+      }
+      return token
+    },
+    session({ session, token }) {
+      session.user.id = token.id
+      return session
+    },
+  },
   // callbacks: {
   //   jwt({ token, user }) {
   //     if (user) { // User is available during sign-in
